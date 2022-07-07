55f8f03db5a34b9744d622ff38e3fc21ffb55168
midou0009 <107584347+midou0009@users.noreply.github.com>
2022-07-07 21:13:54 +0200

    Add files via upload

---
+++ products.js
@@ -0,0 +1,18 @@
+const products = [
+	{
+		id: 2,
+		logo:'img/logo.png',
+		image: 'img/products/1.png',
+		image2: 'img/products/2.png',
+		image3: 'img/products/3.png',
+		image4: 'img/products/4.png',
+		image5: 'img/products/5.png',
+		title: '  برادة متحولة 48 لتر 4في1 مع طاولة سفر مدمجة وكراسي وعجلات لنقل سهل Ferdi Glacière Portative 48L Avec Table, 2 Chaise Ideal Pour Camping ',
+		price1: 8900,
+		price: 7900,
+		description: 'ثلاجة محمولة مثالية لإبقاء مأكولاتك طازجة بل وتبريدها أيضا أثناء التخييم، النزهات أو السفر، بسعة 48 لتر، مناسبة أيضا لتبريد زجاجات المشروبات أو الماء حتى سعة 2 لتر، حيث تتحول لتصبح طاولة سفر محمولة. كما تأتي مع عجلات وحامل قابل للتعديل لنقل سهل، ما يجعلها من القطع الأساسية أثناء رحلات التخييم والتنزه.'   ,
+		phone: 0540693874,
+	},
+
+	
+];
